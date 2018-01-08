03e78d9ab8019c725dea5a7b835c1fd9c98e5578
Lina C <linaclark96@gmail.com>
2018-01-07 22:10:46 -0500

    Finished Game

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -10,13 +10,52 @@ class App extends Component {
   state = {
     bears,
     score: 0,
-    shuffled: [],
+    clickedImages: []
   };
 
-  /*shuffleImages = () => {
-    this.state.bears.map(bears.id => )
-    this.setState({shuffled: this.state.bears});
-  }*/
+  playGame = id =>{
+    //console.log(id.target.id);
+    if(this.state.clickedImages.includes(id.target.id)=== false){
+      this.setState(
+        {
+          score: this.state.score + 1,
+          clickedImages: [...this.state.clickedImages, id.target.id],
+          
+        })
+        console.log(this.state.clickedImages);
+    }else{
+      this.setState(
+        {
+          score: 0,
+          clickedImages: []
+        }
+    )}
+    this.randomize();
+    
+    };
+
+
+  randomize = id => {
+    this.setState(
+      {
+        bears: this.shuffleImages(this.state.bears)
+      })
+  };
+
+  shuffleImages = (arr) => {
+    var currentIndex = arr.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+      
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      
+      temporaryValue = arr[currentIndex];
+      arr[currentIndex] = arr[randomIndex];
+      arr[randomIndex] = temporaryValue;
+    }
+    return arr;
+  };
 
   render() {
     return (
@@ -26,7 +65,8 @@ class App extends Component {
           <Wrapper>
             {this.state.bears.map(bear => (
               <BearCard
-                //shuffle={this.shuffleImages}
+                playGame = {this.playGame}
+                id={bear.id}
                 key={bear.id} 
                 image={bear.image} 
                 name={bear.name}
